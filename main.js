d596a063e302dc068b5066d1e488f09a607149db
JO-GitHub23 <155160104+JO-GitHub23@users.noreply.github.com>
2025-06-03 15:00:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,23 @@ function calculate()
     let oheg = ohegcm / 100;
 
     let bmi = oweg / (oheg * oheg);
+    let type = "";
 
+    if (bmi < 18.5){
+        type = "Underweight";
+    }
+    else if (bmi >= 18.5 && bmi <24.5){
+        type = "Normal Weight";
+    }
+    else if (bmi >= 25 && bmi <29.9){
+        type = "Over Weight";
+    }
+    else {
+        type = "Obese";
+    }
+    
     document.getElementById("result").innerHTML =
-        `Your BMI is : ${bmi.toFixed(2)}`;
-
+        `Your BMI is : ${bmi.toFixed(2)}
+         Your are : ${type}`
+        ;
 }
